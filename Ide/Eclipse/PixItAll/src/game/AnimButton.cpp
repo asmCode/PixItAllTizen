0f79faf5e948594aab567a75d935c44948db3efa
majak <majakthecoder@gmail.com>
2013-12-03 03:11:22 +0100

    fix guzika, ikona

--- Ide/Eclipse/PixItAll/src/game/AnimButton.cpp
+++ Ide/Eclipse/PixItAll/src/game/AnimButton.cpp
@@ -24,6 +24,7 @@ AnimButton::AnimButton(float x, float y,
 					   TexPart normalState,
 					   TexPart pushedState)
 {
+	m_checked = false;
 	this->x = x;
 	this->y = y;
 	this->width = normalState.ImageRect.Width;
